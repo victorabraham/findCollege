bd1dabbb4e1703dab137700288f14db161089d78
Victor Abraham <vicabraham@deloitte.com>
2016-04-19 17:39:13 -0600

    First commit with local mongoDB

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/thelist', function(req,res){
+    var MongoClient = mongodb.MongoClient;
+    var url = 'mongodb://localhost:27017/sampsite';
+    
+    
+    MongoClient.connect(url,function(err,db){
+        if(err){
+            console.log('Unable to connect to server', err);    
+        }else{
+            console.log('Connected to', url);
+            var collection = db.collection('students');
+            
+            collection.find({}).toArray(function (err,result){
+                if(err){
+                    res.send(err);    
+                }else if(result.length){
+                    res.render('studentlist',{
+                        "studentlist":result
+                    });    
+                }else{
+                    res.send('No document found');    
+                }
+                db.close();
+            });
+        }
+    });
+});
+
+module.exports = router;
