050e5b89d7e50e54b58e08795d3bad4c823ccdc4
Victor Abraham <vicabraham@deloitte.com>
2016-04-20 00:28:37 -0600

    Updated title and gitignore files

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,30 @@ var mongodb = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Search College' });
 });
 
 router.get('/thelist', function(req,res){
     var MongoClient = mongodb.MongoClient;
     var url = 'mongodb://localhost:27017/sampsite';
-    
-    
+
+
     MongoClient.connect(url,function(err,db){
         if(err){
-            console.log('Unable to connect to server', err);    
+            console.log('Unable to connect to server', err);
         }else{
             console.log('Connected to', url);
             var collection = db.collection('students');
-            
+
             collection.find({}).toArray(function (err,result){
                 if(err){
-                    res.send(err);    
+                    res.send(err);
                 }else if(result.length){
                     res.render('studentlist',{
                         "studentlist":result
-                    });    
+                    });
                 }else{
-                    res.send('No document found');    
+                    res.send('No document found');
                 }
                 db.close();
             });
