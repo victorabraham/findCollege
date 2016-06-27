4cbffe43a731b032154717404c46f23fd2c777af
Victor Abraham <vicabraham@deloitte.com>
2016-06-27 17:24:07 -0600

    Added fallback for database_url config variable

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ dotenv.load();
 
 module.exports = {
     client: 'postgresql',
-    connection: {
+    connection: process.env.DATABASE_URL || {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
